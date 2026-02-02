2effb06a413a2ec44df83563237f102ba1e4a8c7
Mohamed-Mohamed01 <mohamedcise585@gmail.com>
2026-02-01 17:29:06 -0800

    Updated enemy.js

--- game/enemy.js
+++ game/enemy.js
@@ -326,22 +326,7 @@ class Enemy {
         ctx.restore();
       }
     }
-  }
-  
-export class Enemy {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.width = 32;
-    this.height = 32;
-  }
-
-  update(player) {
-    // later: move toward player
-  }
-
-  draw(ctx) {
-    ctx.fillStyle = "red";
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-  }
 }
+  
+
+
